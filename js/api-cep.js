55a621b7014930e573a9bb455bdacbcd50cabbce
L.F~ <110194660+luizfxz@users.noreply.github.com>
2023-03-02 16:56:44 -0300

    att-02-03

--- js/api-cep.js
+++ js/api-cep.js
@@ -1,6 +1,7 @@
 let cepUser = document.querySelector('#cepUser');
-let cepBusca = document.querySelector('#cepBusca');
-
+let cepBusca = document.querySelector('#cepBuscaUser');
+let numberHome = document.querySelector("#numberHome");
+let infoHome = document.querySelector("#infoHome");
 
 
 cepUser.addEventListener('blur', function(e) {
@@ -16,8 +17,8 @@ cepUser.addEventListener('blur', function(e) {
 
 function popularForm (resposta) {
 
-    console.log(resposta);
-
-    cepBusca.value = resposta.logradouro + resposta.bairro + resposta.localidade + resposta.uf;
+    console.log(numberHome);
+    
+    cepBusca.value = resposta.logradouro + ", " + String(numberHome) + resposta.bairro + ", " + resposta.localidade + ", " + resposta.uf;
 
 }
--- js/modal-card.js
+++ js/modal-card.js
@@ -2,20 +2,30 @@
 const openModalButton = document.querySelector("#cardCredit");
 const closeModalButton = document.querySelector("#close-modal");
 
+// Objeto modal
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
 
 //Modal do pix
 const openPix = document.querySelector("#pixPag");
 const closePix = document.querySelector("#pixCloseModal");
 
-
-// Objeto modal
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
-
-
 // Objeto modal pix
 const modalPix = document.querySelector("#modalPix");
 const fadePix = document.querySelector("#fadePix");
+
+// modal do boleto bancário
+
+const openBol = document.querySelector("#bolPag")
+const bolClose = document.querySelector("#bolCloseModal");
+
+
+//Objeto do boleto bancário 
+
+const modalBol = document.querySelector("#modalBol");
+const fadeBol = document.querySelector("#fadeBol");
+
+
 
 
 // Abrir/fechar o modal
@@ -43,5 +53,20 @@ const toggleModalPix = () => {
 [openPix, closePix, fadePix].forEach((pix) => {
 
     pix.addEventListener("click", () => toggleModalPix());
+    
+});
+
+
+
+const toggleModalBol = () => {
+
+    [modalBol, fadeBol].forEach((bol) => bol.classList.toggle("hide"));
+
+
+};
+
+[openBol, bolClose, fadeBol].forEach((bol) => {
+
+    bol.addEventListener("click", () => toggleModalBol());
     
 });
